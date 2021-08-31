fefa5c7586e0c86cdc683498d72e5465dbbc22b2
Damian Soto <soto.platero@gmail.com>
2021-08-30 21:00:38 -0400

    clear files

--- src/routes/emojis.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	let codes = []
-	for (var i = 12000; i >= 9000; i--) {
-		codes = [...codes, i]
-	}
-</script>
-{#each codes as code, index}
-	<div class="text-lg">{code} - {@html `&#${code};`}</div>
-{/each}
